0088d5f35ac040e56f8d8a9eedad6a51fd1bb29e
GevorgNanyan <nanyan16@outlook.com>
2016-11-10 21:29:24 +0400

    Initial Commit

---
+++ ShareDiary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShareDiary
+//
+//  Created by macbook on 10.11.16.
+//  Copyright © 2016 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
